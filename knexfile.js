6b89d44738ced31b4931f319554a2489ae22d00b
Amber Sorensen <ambersorensen@ambers-mbp.lan>
2019-11-18 18:24:34 -0700

    knex file bugs

--- knexfile.js
+++ knexfile.js
@@ -1,52 +1,32 @@
-const dbConnection = process.env.DATABASE_URL;
-const bcrypt = require("bcryptjs");
-
 module.exports = {
   development: {
-    client: "sqlite3",
-    connection: {
-      filename: "./data/nannies.db3"
-    },
+    client: 'sqlite3',
     useNullAsDefault: true,
-    // SQLite will not enforce foreign keys by default
+    connection: {
+      filename: './data/datbase.db3'
+    },
     pool: {
       afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
-      }
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
     },
-    migrations: {
-      directory: "./data/migrations"
-    },
-    seeds: {
-      directory: "./data/seeds"
-    }
-  },
-  testing: {
-    client: "sqlite3",
-    connection: {
-      filename: "./data/test.db3"
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: "./data/migrations"
-    },
-    seeds: {
-      directory: "./data/seeds"
-    }
+      migrations: {
+        directory: './data/migrations',
+      },
   },
   production: {
-    client: "pg",
+    client: 'pg',
     useNullAsDefault: true,
     connection: process.env.DATABASE_URL,
-    pool:{
+    pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      directory: "./data/migrations"
+        directory: './data/migrations',
     },
     seeds: {
-      directory: "./data/seeds"
-    }
-  }
-};
+      directory: './data/seeds',
+    },
+  },
+}
